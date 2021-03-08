987ff318f9be3c0e9e4d0afd33304e90daf1782a
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-03-09 06:23:14 +1100

    upload files

--- auth-service/src/main/kotlin/com/vnapnic/auth/model/Account.kt
+++ auth-service/src/main/kotlin/com/vnapnic/auth/model/Account.kt
@@ -2,30 +2,46 @@ package com.vnapnic.auth.model
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.Transient
 import org.springframework.data.mongodb.core.mapping.Document
 
-
-@Document
+@Document(collection = "account")
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(
+        "id",
+        "username",
+        "password",
+        "email",
+        "active")
 data class Account(
-        @JsonProperty("id")
-        var id: String? = null,
 
+        @JsonProperty("id")
+        @Id
+        var id: String = "",
+        @JsonProperty("phoneNumber")
+        var phoneNumber: String? = null,
         @JsonProperty("socialId")
         var socialId: String? = null,
-
-        @JsonProperty("username")
-        var username: String? = null,
-
-        @JsonProperty(value = "password", access = JsonProperty.Access.WRITE_ONLY)
-        var password: String? = null,
-
         @JsonProperty("email")
         var email: String? = null,
-
+        @JsonProperty("password")
+        var password: String? = null,
+        @JsonProperty("cccdFront")
+        var cccdFront: String? = null,
+        @JsonProperty("cccdBack")
+        var cccdBack: String? = null,
         @JsonProperty("active")
-        var active: Boolean? = null,
+        var active: Boolean = false,
+        @JsonProperty("verified") // Email verification passed
+        var verified: Boolean = false,
+        @JsonProperty("verified") // Email verification passed
 
-        @JsonProperty("verified")
-        var verified: Boolean? = null
-)
+        var staffId: String = ""
+) {
+    companion object {
+        @Transient
+        const val SEQUENCE_NAME = "account_sequence"
+    }
+}
