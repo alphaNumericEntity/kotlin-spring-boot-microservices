8efacb60238af12109d0c54305c6f9b548685db8
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-08-07 10:04:53 +1000

    a

--- common/src/main/kotlin/com/vnapnic/common/service/SequenceGeneratorService.kt
+++ common/src/main/kotlin/com/vnapnic/common/service/SequenceGeneratorService.kt
@@ -1,14 +1,13 @@
 package com.vnapnic.common.service
 
+import com.vnapnic.common.db.SequenceId
 import com.vnapnic.common.exception.SequenceException
-import com.vnapnic.database.beans.SequenceIdBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.core.FindAndModifyOptions
 import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
-import kotlin.jvm.Throws
 
 interface SequenceGeneratorService {
     fun nextSequenceId(key: String): Int?
@@ -34,10 +33,10 @@ open class SequenceGeneratorServiceImpl : SequenceGeneratorService {
         // modify in document
         //if no id, throws SequenceException
         //optional, just a way to tell user when the sequence id is failed to generate.
-        val sequenceId: SequenceIdBean = mongoOperations
+        val sequenceId: SequenceId = mongoOperations
                 .findAndModify(query,
                         update, options,
-                        SequenceIdBean::class.java)
+                        SequenceId::class.java)
                 ?: throw SequenceException("Unable to get sequence id for key : $key")
 
         return sequenceId.seq
