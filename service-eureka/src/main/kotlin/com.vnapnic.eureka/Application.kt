5bda7c287dc56235876151eb5a7d4229a172fa33
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-08-31 03:17:03 +1000

    refactor source

--- service-eureka/src/main/kotlin/com.vnapnic.eureka/Application.kt
+++ service-eureka/src/main/kotlin/com.vnapnic.eureka/Application.kt
@@ -1,9 +1,11 @@
-package com.vnapnic.user
+package com.vnapnic.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableEurekaServer
 class Application
 
 fun main(args: Array<String>) {
