8b91040b27ed62103b71a46fd3403171658a1114
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-10-17 01:39:29 +1100

    create service user

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,8 +1,8 @@
 dependencies {
     implementation(project(":common"))
 
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.cloud:spring-cloud-config-client")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation("org.springframework.session:spring-session-data-redis")
-    implementation("org.springframework.data:spring-data-redis")
 }
