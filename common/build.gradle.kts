ba681f901d78d6402d441ff7845c047ecd7625af
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-10-20 11:46:11 +1100

    jwt

---
+++ common/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.data:spring-data-commons")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
