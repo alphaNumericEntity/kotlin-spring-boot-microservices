897e324534d9718de7c2095f9bf20ec0456cbfff
alphaNumericEntity <71961814+alphaNumericEntity@users.noreply.github.com>
2021-12-14 02:49:33 +1100

    update

--- user-service/src/main/kotlin/com/vnapnic/user/repositories/UserRepository.kt
+++ user-service/src/main/kotlin/com/vnapnic/user/repositories/UserRepository.kt
@@ -1,4 +1,10 @@
 package com.vnapnic.user.repositories
 
-interface UserRepository {
+import com.vnapnic.common.db.Account
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : MongoRepository<Account, String> {
+
 }
